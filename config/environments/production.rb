e852726dafd234504588a99839bdc609c215e5df
ProPillow <wilsonshum96@hotmail.com>
2018-06-11 23:15:20 -0700

    Some helpful message for your future self

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,7 @@ Rails.application.configure do
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.smtp_settings = {
       address:              'smtp.gmail.com',
-      port:                 587,
+      port:                 587, 
       domain:               'gmail.com',
       user_name:            'wilsonwebsite123@gmail.com', #change user
       password:             'Asdfghjkl0)', #change password
